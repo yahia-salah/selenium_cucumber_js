6aa339a320d4306cbae339b5b31ef742bce7b6c3
Yahia Salah <ysalah@integrant.com>
2023-06-09 17:21:41 +0300

    Adding Reporting HTML and Examples

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,36 +1,51 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { After, Before } = require("@cucumber/cucumber");
 const chai = require("chai");
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const {Options} = require('selenium-webdriver/chrome');
+const { Builder, By, Key, until } = require("selenium-webdriver");
+const { Options } = require("selenium-webdriver/chrome");
 
-var options = new Options();
-options.addArguments("--start-maximized");
-let driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();;
+let driver;
 
 var expect = chai.expect;
 
-Before(async function(){
-  console.log("Inside Before!")
-  await driver.manage().setTimeouts({implicit:5000,pageLoad:10000,script:3000});
+Before(async function () {
+  console.log("Inside Before!");
+  var options = new Options();
+  options.addArguments("--start-maximized","--ignore-certificate-errors");
+  driver = new Builder().forBrowser("chrome").setChromeOptions(options).build();
+  await driver
+    .manage()
+    .setTimeouts({ implicit: 5 * 1000, pageLoad: 30 * 1000, script: 3 * 1000 });
 });
 
-After(async function(){
-  console.log("Inside After!")
+After(async function () {
+  console.log("Inside After!");
   await driver.quit();
 });
 
-Given("I am navigated to {string}", async function (url) {
-  await driver.get(url);
-});
+Given(
+  "I am navigated to {string}",
+  { timeout: 30 * 1000 },
+  async function (url) {
+    await driver.get(url);
+  }
+);
 
 When("I select Country as {string}", async function (country) {
   await driver.findElement(By.css("a#country-btn")).click();
-  await driver.findElement(By.xpath(`//a[contains(@class,'country')]/span[contains(text(),'${country}')]/parent::a`)).click();
+  await driver
+    .findElement(
+      By.xpath(
+        `//a[contains(@class,'country')]/span[contains(text(),'${country}')]/parent::a`
+      )
+    )
+    .click();
 });
 
 Then("I verify the currency is {string}", async function (currency) {
-  var trialCostText = await driver.findElement(By.css("div.trial-cost")).getText();
+  var trialCostText = await driver
+    .findElement(By.css("div.trial-cost"))
+    .getText();
   expect(trialCostText).to.contain(currency);
 });
 
@@ -38,8 +53,12 @@ Then("I verify the prices of each package", async function (dataTable) {
   var types = dataTable.hashes().map((x) => x.Type);
   var prices = dataTable.hashes().map((x) => x.Price);
 
-  for(var i=0;i<types.length;i++){
-    var actualPriceText = await driver.findElement(By.xpath(`//div[@class='price' and contains(@id,'${types[i]}')]`)).getText();
+  for (var i = 0; i < types.length; i++) {
+    var actualPriceText = await driver
+      .findElement(
+        By.xpath(`//div[@class='price' and contains(@id,'${types[i]}')]`)
+      )
+      .getText();
     var actualPrice = actualPriceText.split(" ")[0].trim();
     expect(actualPrice).to.equal(prices[i]);
   }
--- features/test.feature
+++ features/test.feature
@@ -1,11 +1,17 @@
 Feature: STCTV Subscription Packages
 
-    Scenario: Validate the Subscription Packages - Type & Price and Currency for all Countries
+    Scenario Outline: Validate the Subscription Packages - Type & Price and Currency for <Country>
         Given I am navigated to "https://subscribe.stctv.com/"
-        When I select Country as "عمان"
-        Then I verify the currency is "ريال عماني"
+        When I select Country as "<Country>"
+        Then I verify the currency is "<Currency>"
         And I verify the prices of each package
             | Type     | Price |
-            | لايت     | 5     |
-            | الأساسية | 10    |
-            | بريميوم  | 15    |
+            | لايت     | <Price_1>     |
+            | الأساسية | <Price_2>    |
+            | بريميوم  | <Price_3>    |
+
+Examples:
+    | Country | Currency | Price_1 |Price_2|Price_3|
+    | عمان | ريال عماني | 5 |10|15|
+    | الإمارات | درهم إماراتي | 5.4 |10.9|16.3|
+    | الأردن | دينار أردني | 2.7 |5.2|8|
